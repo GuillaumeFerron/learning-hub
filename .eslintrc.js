635d3c02d3d7addf4efc7a797a041617d9920e6a
GuillaumeFerron <guillaume.ferron2@gmail.com>
2018-11-03 20:46:47 -0400

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  extends: [
+    // add more generic rulesets here, such as:
+    'plugin:vue/essential',
+    'airbnb'
+  ],
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'comma-dangle': 'off',
+    'space-before-function-paren': 'off',
+    'import/no-unresolved': 'off',
+    'object-curly-spacing': 'off',
+    'no-extra-boolean-cast': 'off',
+    'max-len': ["error", {"code": 150}],
+    'vue/script-indent': ["error", 2, {"baseIndent": 1}]
+  },
+  "overrides": [
+    {
+      "files": ["*.js"],
+      "rules": {
+        "vue/script-indent": "off"
+      }
+    },
+    {
+      "files": ["*.vue"],
+      "rules": {
+        "indent": "off"
+      }
+    }
+  ]
+};
