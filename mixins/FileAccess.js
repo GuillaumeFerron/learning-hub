a38a68bae7e82cf83397fa64ac6c2e3fc6ac2d90
GuillaumeFerron <guillaume.ferron2@gmail.com>
2018-11-21 22:30:31 -0500

    Small fix

--- mixins/FileAccess.js
+++ mixins/FileAccess.js
@@ -1,5 +1,10 @@
 export default {
   computed: {
+    /**
+     * Returns the fetchable url from a directory, file name and file extension
+     *
+     * @returns {string}
+     */
     fileUrl() {
       return `${this.directory}${this.file_name}${this.file_extension}`
     }
--- mixins/Navigation.js
+++ mixins/Navigation.js
@@ -1,7 +1,13 @@
 export default {
   filters: {
+    /**
+     * Makes a lower case string first uppercased
+     *
+     * @param name
+     * @returns {string}
+     */
     navigationFormat(name) {
-      return name.charAt(0).toUpperCase() + name.slice(1)
+      return name ? name.charAt(0).toUpperCase() + name.slice(1) : ''
     }
   }
 }
