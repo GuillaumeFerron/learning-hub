1d62792985bd255a239f177d40c51780f0fcd19a
PK <parham.kargar@gmail.com>
2019-06-16 20:03:54 -0400

    add troubleshooting and hub api video

--- store/videos/index.js
+++ store/videos/index.js
@@ -10,7 +10,29 @@ const videosState = () => ({
         author: 'PK',
         length: '00:01:37'
       },
-      tags: ['animation', 'extension', 'css']
+      tags: ['animation', 'extension', 'css', 'Learn Hub']
+    },
+    troubleshooting: {
+      name: 'Troubleshooting Older Nuxt.js Framework',
+      file: 'troubleshooting',
+      file_extension: '.mp4',
+      description: 'In this screencast we will be troubleshooting how to get Learning hub working',
+      meta: {
+        author: 'PK',
+        length: '00:12:01'
+      },
+      tags: ['Nuxt.js', 'troubleshooting', 'nodejs', 'npm']
+    },
+    learnHubAuth01: {
+      name: 'Learn Hub Authentication - 01 Adding Laravel',
+      file: 'learnHubAuth01',
+      file_extension: '.mp4',
+      description: 'In this Video We will be installing laravel and cleaning up framework to add API Authentication',
+      meta: {
+        author: 'PK',
+        length: '00:12:01'
+      },
+      tags: ['Nuxt.js', 'Laravel', 'Auth', 'Learn Hub']
     }
   }
 })
